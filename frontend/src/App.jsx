a86226c6dfa8ec6c92aeda387ea3069b050c578b
Official-Vinit <officialvinit1176@gmail.com>
2026-08-19 16:25:01 +0530

    add: ai images and card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import {React,useContext} from 'react'
+import { React, useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Customize from './pages/Customize'
 import { userDataContext } from './context/UserContext'
+import Home from './pages/Home'
 
 
 function App() {
-  const {userData,setUserData}=useContext(userDataContext)
+  const { userData, setUserData } = useContext(userDataContext)
   return (
     <Routes>
-        <Route path="/signup" element={<SignUp />} />
+      <Route path='/' element={(userData?.assistantImage && userData?.assistantName)? <Home/> :<Navigate to={"/customize"}/>}/>
+      <Route path='/signup' element={!userData ? <SignUp /> : <Navigate to={"/"} />} />
+      <Route path='/signin' element={!userData ? <SignIn /> : <Navigate to={"/"} />} />
+      <Route path='/customize' element={userData ? <Customize /> : <Navigate to={"/signup"} />} />
+
     </Routes>
   )
 }
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,24 +1,30 @@
-
-import React, { createContext,useState} from 'react'
+import axios from 'axios'
+import React, { createContext, useEffect, useState } from 'react'
 export const userDataContext = createContext()
 function UserContext({ children }) {
-    const serverUrl = "http://localhost:5000"
-
-    const [userData,setUserData]=useState(null)
-
-    const handleCurrentUser=async ()=>{
+    const serverUrl = "http://localhost:8000"
+    const [userData, setUserData] = useState(null)
+    const [frontendImage, setFrontendImage] = useState(null)
+    const [backendImage, setBackendImage] = useState(null)
+    const [selectedImage, setSelectedImage] = useState(null)
+    const handleCurrentUser = async () => {
         try {
-            const result=await axios.get(`${serverUrl}/api/user/current`,{withCredentials:true})
+            const result = await axios.get(`${serverUrl}/api/user/current`, { withCredentials: true })
             setUserData(result.data)
             console.log(result.data)
         } catch (error) {
             console.log(error)
         }
     }
-
+    useEffect(() => {
+        handleCurrentUser()
+    }, [])
+    const value = {
+        serverUrl, userData, setUserData, backendImage, setBackendImage, frontendImage, setFrontendImage, selectedImage, setSelectedImage
+    }
     return (
         <div>
-            <userDataContext.Provider value={{ userData, setUserData, serverUrl, handleCurrentUser }}>
+            <userDataContext.Provider value={value}>
                 {children}
             </userDataContext.Provider>
         </div>
