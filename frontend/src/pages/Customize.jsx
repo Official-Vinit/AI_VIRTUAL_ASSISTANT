e5d8db0c8503d03ec48546467ef76822b47a5306
Official-Vinit <officialvinit1176@gmail.com>
2026-08-19 16:47:03 +0530

    add: customize agent pages

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -1,47 +1,52 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
+import Card from '../components/Card'
+import image1 from "../assets/image1.png"
+import image2 from "../assets/image2.jpg"
+import image3 from "../assets/authBg.png"
+import image4 from "../assets/image4.png"
+import image5 from "../assets/image5.png"
+import image6 from "../assets/image6.jpeg"
+import image7 from "../assets/image7.jpeg"
+import { RiImageAddLine } from "react-icons/ri";
 import { userDataContext } from '../context/UserContext'
-import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { MdKeyboardBackspace } from "react-icons/md";
-import { useNavigate } from 'react-router-dom';
-function Customize2() {
-    const {userData,backendImage,selectedImage,serverUrl,setUserData}=useContext(userDataContext)
-    const [assistantName,setAssistantName]=useState(userData?.AssistantName || "")
-    const [loading,setLoading]=useState(false)
-    const navigate=useNavigate()
+function Customize() {
+    const { serverUrl, userData, setUserData, backendImage, setBackendImage, frontendImage, setFrontendImage, selectedImage, setSelectedImage } = useContext(userDataContext)
+    const navigate = useNavigate()
+    const inputImage = useRef()
 
-    const handleUpdateAssistant=async ()=>{
-        setLoading(true)
-        try {
-            let formData=new FormData()
-            formData.append("assistantName",assistantName)
-            if(backendImage){
-                 formData.append("assistantImage",backendImage)
-            }else{
-                formData.append("imageUrl",selectedImage)
-            }
-            const result=await axios.post(`${serverUrl}/api/user/update`,formData,{withCredentials:true})
-setLoading(false)
-            console.log(result.data)
-            setUserData(result.data)
-            navigate("/")
-        } catch (error) {
-            setLoading(false)
-            console.log(error)
-        }
+    const handleImage = (e) => {
+        const file = e.target.files[0]
+        setBackendImage(file)
+        setFrontendImage(URL.createObjectURL(file))
     }
+    return (
+        <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col p-[20px] '>
+            <MdKeyboardBackspace className='absolute top-[30px] left-[30px] text-white cursor-pointer w-[25px] h-[25px]' onClick={() => navigate("/")} />
+            <h1 className='text-white mb-[40px] text-[30px] text-center '>Select your <span className='text-blue-200'>Assistant Image</span></h1>
+            <div className='w-full max-w-[900px] flex justify-center items-center flex-wrap gap-[15px]'>
+                <Card image={image1} />
+                <Card image={image2} />
+                <Card image={image3} />
+                <Card image={image4} />
+                <Card image={image5} />
+                <Card image={image6} />
+                <Card image={image7} />
+                <div className={`w-[70px] h-[140px] lg:w-[150px] lg:h-[250px] bg-[#020220] border-2 border-[#0000ff66] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-950 cursor-pointer hover:border-4 hover:border-white flex items-center justify-center ${selectedImage == "input" ? "border-4 border-white shadow-2xl shadow-blue-950 " : null}`} onClick={() => {
+                    inputImage.current.click()
+                    setSelectedImage("input")
+                }}>
+                    {!frontendImage && <RiImageAddLine className='text-white w-[25px] h-[25px]' />}
+                    {frontendImage && <img src={frontendImage} className='h-full object-cover' />}
 
-  return (
-    <div className='w-full h-[100vh] bg-gradient-to-t from-[black] to-[#030353] flex justify-center items-center flex-col p-[20px] relative '>
-        <MdKeyboardBackspace className='absolute top-[30px] left-[30px] text-white cursor-pointer w-[25px] h-[25px]' onClick={()=>navigate("/customize")}/>
-      <h1 className='text-white mb-[40px] text-[30px] text-center '>Enter Your <span className='text-blue-200'>Assistant Name</span> </h1>
-      <input type="text" placeholder='eg. shifra' className='w-full max-w-[600px] h-[60px] outline-none border-2 border-white bg-transparent  text-white placeholder-gray-300 px-[20px] py-[10px] rounded-full text-[18px]' required onChange={(e)=>setAssistantName(e.target.value)} value={assistantName}/>
-      {assistantName &&  <button className='min-w-[300px] h-[60px] mt-[30px] text-black font-semibold cursor-pointer  bg-white rounded-full text-[19px] ' disabled={loading} onClick={()=>{
-        handleUpdateAssistant()
-    }
-        } >{!loading?"Finally Create Your Assistant":"Loading..."}</button>}
-     
-    </div>
-  )
+                </div>
+                <input type="file" accept='image/*' ref={inputImage} hidden onChange={handleImage} />
+            </div>
+            {selectedImage && <button className='min-w-[150px] h-[60px] mt-[30px] text-black font-semibold cursor-pointer  bg-white rounded-full text-[19px] ' onClick={() => navigate("/customize2")}>Next</button>}
+
+        </div>
+    )
 }
 
-export default Customize2
+export default Customize
