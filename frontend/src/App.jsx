4d8c5a3b9d13723f816dbfafde9c3745f53a5bd9
Official-Vinit <officialvinit1176@gmail.com>
2026-08-19 15:56:18 +0530

    add: login & signup page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import { userDataContext } from './context/UserContext'
+
+
+function App() {
+  const {userData,setUserData}=useContext(userDataContext)
+  return (
+    <Routes>
+        <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  )
+}
+
+export default App
