594324c575aafdcfa0cd0faf0998894e94329a03
Official-Vinit <officialvinit1176@gmail.com>
2026-08-19 17:55:03 +0530

    add: webspeech api setup

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import React, { createContext, useEffect, useState } from 'react'
 export const userDataContext = createContext()
 function UserContext({ children }) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "http://localhost:5000"
     const [userData, setUserData] = useState(null)
     const [frontendImage, setFrontendImage] = useState(null)
     const [backendImage, setBackendImage] = useState(null)
@@ -16,11 +16,21 @@ function UserContext({ children }) {
             console.log(error)
         }
     }
+
+    const getGeminiResponse = async (command) => {
+        try {
+            const result = await axios.post(`${serverUrl}/api/user/asktoassistant`, { command }, { withCredentials: true })
+            return result.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
         handleCurrentUser()
     }, [])
     const value = {
-        serverUrl, userData, setUserData, backendImage, setBackendImage, frontendImage, setFrontendImage, selectedImage, setSelectedImage
+        serverUrl, userData, setUserData, backendImage, setBackendImage, frontendImage, setFrontendImage, selectedImage, setSelectedImage, getGeminiResponse
     }
     return (
         <div>
